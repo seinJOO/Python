05ba5c221aa941a559dbd11666077661ba863d9e
seinJOO <sein19950910@gmail.com>
2022-03-29 17:30:38 +0900

    0329 class

---
+++ 0329/calc.py
@@ -0,0 +1,17 @@
+
+
+# 모듈 만들기
+# 두 수를 입력받아 연산하는 계산기 모듈
+# Sum(a,b) / Sub(a,b) / Mul(a,b) / Div(a,b)
+
+def Sum(a,b) :
+    return a+b
+
+def Sub(a,b) :
+    return a-b
+
+def Mul(a,b) :
+    return a*b
+
+def Div(a,b) :
+    return a/b
