04ddccc92c6b30cd7e88f3c58e0375c3104ff571
seinJOO <sein19950910@gmail.com>
2022-03-30 17:27:06 +0900

    0330 class

---
+++ 0330/example.py
@@ -0,0 +1,10 @@
+from myCalcPackage.calcModule import Add,Sub,Mul,Div
+
+x = 10
+y = 5
+
+print(f'x = {x}; y = {y} 일 때')
+print('Add =',Add(x, y))
+print('Sub =',Sub(x, y))
+print('Mul =',Mul(x, y))
+print('Div =',Div(x, y))
